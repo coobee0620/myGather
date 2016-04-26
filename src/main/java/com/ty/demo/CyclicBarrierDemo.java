70c866cf55a9dc5667454181ce28f1ba3dc3464d
tian yu <kangtatk@sina.com>
2016-04-26 14:04:19 +0800

    增加CountDownLatch和CyclicBarrier演示

---
+++ src/main/java/com/ty/demo/CyclicBarrierDemo.java
@@ -0,0 +1,37 @@
+package com.ty.demo;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created by tianyu on 16/4/26.
+ */
+public class CyclicBarrierDemo implements Runnable{
+    private CyclicBarrier cyclicBarrier;
+    private int id;
+
+    public CyclicBarrierDemo(int id, CyclicBarrier cyclicBarrier) {
+        this.cyclicBarrier = cyclicBarrier;
+        this.id = id;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println("玩家" + id + "正在玩第一关...");
+            cyclicBarrier.await();
+            System.out.println("玩家" + id + "进入第二关...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(4, () -> System.out.println("所有玩家完成第一关！"));
+
+        for (int i = 0; i < 4; i++) {
+            new Thread(new CyclicBarrierDemo(i, cyclicBarrier)).start();
+        }
+    }
+}
